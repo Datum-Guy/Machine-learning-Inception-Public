a4f5df3be6e1dc8cb5860fd6bbb47e9fdce41c40
Datum-Guy <datumguy.team@gmail.com>
2020-03-08 03:05:32 +0530

    Updated Server.sh

--- jupyter-server.sh
+++ jupyter-server.sh
@@ -7,3 +7,4 @@ jupyter notebook --generate-config
 wget https://raw.githubusercontent.com/Datum-Guy/Machine-learning-Inception-Public/master/jupyter_notebook_config.py
 mv ~/jupyter_notebook_config.py ~/.jupyter/
 sudo pip install numpy scipy pandas sklearn matplotlib
+sudo pip3 install numpy scipy pandas sklearn matplotlib
